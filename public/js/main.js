1720d71b7876020abb0d517febdcae0b81563dbc
Jesse Ditson <jesse.ditson@gmail.com>
2016-01-13 13:19:11 -0800

    finish up first revision of docs

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,30 @@
 
-// window.onload = function() {
-//   window.addEventListener('scroll', function() {
-//     let top = (window.pageYOffset || document.scrollTop) - (document.clientTop || 0)
-//     let nav = document.querySelector('navigation')
-//     nav.style.top = top + 'px'
-//     console.log(top)
-//   })
-// }
+window.onload = function() {
+  window.addEventListener('scroll', requestTick, false)
+
+  var nav = document.querySelector('navigation')
+  var body = document.querySelector('body')
+  nav.addEventListener('mouseenter', function() {
+    body.className += ' nav-open'
+  })
+  nav.addEventListener('mouseleave', function() {
+    body.className = body.className.split(' ').filter(c => c != 'nav-open').join(' ')
+  })
+}
+
+function windowTop() {
+  return (window.pageYOffset || document.scrollTop) - (document.clientTop || 0)
+}
+
+let ticking
+function requestTick() {
+  if (!ticking) {
+    requestAnimationFrame(update)
+    ticking = true
+  }
+}
+function update() {
+  let nav = document.querySelector('.nav-list')
+  nav.style.top = windowTop() + 'px'
+  ticking = false
+}
